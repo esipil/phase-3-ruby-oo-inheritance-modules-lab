ec55ef393b22eeea600d85d02a0416aabc28a80c
Timothy <lemayiant4@gmail.com>
2022-11-14 15:32:21 +0300

    completed assignments

---
+++ lib/concerns/findable.rb
@@ -0,0 +1,8 @@
+
+
+module Findable
+    def find_by_name(name)
+        all.detect{|a| a.name == name}   #filtering the artist by name to call this we do class nama i find by name and pass in a name 
+      end
+    
+end
